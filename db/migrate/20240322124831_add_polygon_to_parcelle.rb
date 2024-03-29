25d98408f299b0504e269ee36e340c5a88681cee
RaphaelAubry <raphael.aubry@hotmail.fr>
2024-03-29 14:03:43 +0100

    postgis

--- db/migrate/20240322124831_add_polygon_to_parcelle.rb
+++ db/migrate/20240322124831_add_polygon_to_parcelle.rb
@@ -1,6 +1,5 @@
 class AddPolygonToParcelle < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'postgis'
     add_column :parcelles, :polygon, :st_polygon, geographic: true
   end
 end
