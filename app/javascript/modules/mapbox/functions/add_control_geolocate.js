2ad12b845643d9e1a812b2826d53dbe54a7a1237
Raphael Aubry <raphael.aubry@hotmail.fr>
2026-01-15 12:29:04 +0100

    geolocalisation accuracy

--- app/javascript/modules/mapbox/functions/add_control_geolocate.js
+++ app/javascript/modules/mapbox/functions/add_control_geolocate.js
@@ -1,18 +1,58 @@
-import mapboxgl from "mapbox-gl"
-import { wait } from "modules/wait"
+import mapboxgl from "mapbox-gl" 
+import { wait } from "modules/wait" 
 
 mapboxgl.Map.prototype.addControlGeolocate = function () {
-  const geolocate = (new mapboxgl.GeolocateControl({
+  const geolocate = new mapboxgl.GeolocateControl({
     positionOptions: {
-      enableHighAccuracy: true
+      enableHighAccuracy: true,
+      timeout: 15000,
+      maximumAge: 0
     },
     showAccuracyCircle: true,
     trackUserLocation: true,
     showUserHeading: true
-  }))
-  this.addControl(geolocate)
+  }) 
 
-  wait('.mapboxgl-ctrl-geolocate > span').then(element => {
-    element.title = 'Trouver ma position'
-  })
-}
+  this.addControl(geolocate) 
+
+  wait('.mapboxgl-ctrl-geolocate > span').then(el => el.title = 'Trouver ma position') 
+
+  let positions = [] 
+  let lastFlyToTime = 0 
+  const FLY_INTERVAL = 3000  // intervalle minimal entre deux recentrages
+
+  geolocate.on('geolocate', (event) => {
+    const { latitude, longitude, accuracy } = event.coords 
+
+    // Ignorer les positions trop imprécises
+    if (accuracy > 50) return 
+
+    positions.push({ latitude, longitude, accuracy, timestamp: Date.now() }) 
+    if (positions.length > 5) positions.shift() 
+
+    // Moyenne pondérée par précision
+    const weighted = positions.reduce((acc, pos) => {
+      const weight = 1 / pos.accuracy 
+      acc.lat += pos.latitude * weight 
+      acc.lng += pos.longitude * weight 
+      acc.totalWeight += weight 
+      return acc 
+    }, { lat: 0, lng: 0, totalWeight: 0 }) 
+
+    const avgLat = weighted.lat / weighted.totalWeight 
+    const avgLng = weighted.lng / weighted.totalWeight 
+
+    const now = Date.now() 
+    if (now - lastFlyToTime > FLY_INTERVAL) {
+      // Recentre la carte sur la position moyenne
+      this.flyTo({
+        center: [avgLng, avgLat],
+        zoom: 15,
+        speed: 1.2
+      }) 
+      lastFlyToTime = now 
+    }
+  }) 
+
+  return geolocate 
+} 
