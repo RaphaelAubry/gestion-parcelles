12df3c98bf72fc509d6d5fd95c343be1756bf9c4
Raphael Aubry <raphael.aubry@hotmail.fr>
2026-01-16 23:44:19 +0100

    skip authentication mapscontroller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,6 @@
 class MapsController < ApplicationController
+    skip_before_action :authenticate_user!
+
     def token
         respond_to do |format|
           format.json { render json: { token: ENV['MAPBOX_TOKEN'] } }
