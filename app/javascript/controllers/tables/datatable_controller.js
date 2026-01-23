76320d6a5575ade5372f75014a9f5b296ba87ff8
Raphael Aubry <raphael.aubry@hotmail.fr>
2026-01-23 13:11:49 +0100

    datatable

--- app/javascript/controllers/tables/datatable_controller.js
+++ app/javascript/controllers/tables/datatable_controller.js
@@ -1,22 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 import $ from 'jquery'
 import DataTable from "datatables.net"
-import { config } from "modules/datatable/datatable_config"
-import { headers } from "modules/datatable/datatable_headers"
+import { config, headers } from "modules/datatable"
 import "modules/datatable/functions"
 
 export default class extends Controller {
   static values = { name: String }
 
   connect() {
-
     if ($.fn.DataTable.isDataTable(this.element)) return
     
     if (!this.table) {
       this.#createTable()
-      this.table.setColumnNames()
     }
     this.#setColumnsVisibility()
+    // gestion des headers pour la table responsive
+    this.table.setColumnNames(headers)
+
+    const setColumnNamesListener = () => { this.table.setColumnNames(headers) }
+    window.addEventListener('resize',  setColumnNamesListener)
   }
 
   disconnect() {
@@ -58,44 +60,32 @@ export default class extends Controller {
         }
       }
     })
-
-    this.addColumnHeaders(columns)
-    this.updateFooter()
+    
+    // gestion des headers pour la table responsive
+    this.#addColumnHeaders(columns)
+    // la formule total surface
+    this.#updateFooter()
   }
-
-  addColumnHeaders(columns) {
-    this.table.rows().every(function() {
-      this.node().childNodes.forEach((td) => {
-        const colName = columns[td._DT_CellIndex.column].name
-        td.dataset.name = colName
-
-        if (!td.querySelector('th')) {
-          const th = document.createElement('th')
-          th.innerText = headers[colName] || colName
-          td.prepend(th)
-        }
+  
+  // ajout des dataset name pour récupérer les noms de colonnes en vue smartphone
+  #addColumnHeaders(columns) {
+    this.table.on('draw', event => {
+      this.table.rows().every(function() {
+        this.node().childNodes.forEach(td => {
+          const colName = columns[td._DT_CellIndex.column].name
+          td.dataset.name = colName
+        })
       })
     })
   }
 
-  updateFooter() {
+  #updateFooter() {
     this.table.on('draw', event => {
       let elements = document.querySelectorAll('[data-total="surface"]')
   
       elements.forEach(e => {
         e.textContent = event.dt.ajax.json().total_surface
       })
-    })
-  }
-
-  addDatasetColumnName(columns) {
-    this.table.on('draw', event => {
-      table.rows().every(function() {  
-        this.node().childNodes.forEach((td) => {
-          td.setAttribute('data-name', columns[td._DT_CellIndex.column].name)
-        })
-      })
-      this.table.setColumnNames()
     })
   }
 
--- app/javascript/modules/datatable/functions/set_column_names.js
+++ app/javascript/modules/datatable/functions/set_column_names.js
@@ -1,34 +1,43 @@
 import $ from 'jquery'
 import "datatables.net"
 
-$.fn.dataTable.Api.register('setColumnNames', function() {
-   console.log('set')
-   console.log(this)
-    if (window.innerWidth < 768) {
-      console.log('set1')
-      console.log(this.rows())
-      this.rows().every(function(row) {
-        this.node().childNodes.forEach((td) => {
-            if(!td.querySelector('th')) {
-              const name = document.createElement('th')
-              name.innerText = headers[td.dataset.name]
-              td.prepend(name)
-            }
-        })
-      })
-    }
+
+$.fn.dataTable.Api.register('setColumnNames', function(headers) {
+    // capture de l'instance datatable
+    const api = this
     
-    if (window.innerWidth >= 768) {
-      
-      this.rows().every(function(row) {
-        this.node().childNodes.forEach((td) => {
-            if(td.querySelector('th')) {
-              td.querySelector('th').remove()
-            }
-        })
-      })
+    api.on('draw', function () {
+      updateHeaders(api, headers)
+    })   
+    updateHeaders(api, headers)
+    return api
+})
+
+
+function updateHeaders(table, headers) {
+  
+  table.rows().every(function() {
+    for (let td of this.node().childNodes) {
+
+      if (window.innerWidth < 768) {
+        // ajout des headers dans les lignes
+        if(!td.querySelector('th')) {
+          const name = document.createElement('th')
+          
+          if (td.dataset.name != "actions") { // on n'affiche pas le header pour la colonne modifier/supprimer
+            name.innerText = headers[td.dataset.name]
+            td.prepend(name)
+          }
+        }
+      }
+    
+      if (window.innerWidth >= 768) {
+        // suppression des headers dans les lignes
+        if(td.querySelector('th')) {
+            td.querySelector('th').remove()
+        }
+      }
 
     }
-    return this
-  
-})
+  })
+}
