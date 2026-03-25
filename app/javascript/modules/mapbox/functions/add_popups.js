b5eac12d5ec85b6617abfa1879388c41ead4a449
Raphael Aubry <raphael.aubry@hotmail.fr>
2026-03-25 11:18:18 +0100

    tap

--- app/javascript/modules/mapbox/functions/add_popups.js
+++ app/javascript/modules/mapbox/functions/add_popups.js
@@ -5,30 +5,40 @@ mapboxgl.Map.prototype.addPopupsManager = function() {
   this.popupsManager = new PopupsManager
 }
 
-// manage popups for parcelle registered
 mapboxgl.Map.prototype.addPopups = function () {
   const popup = new mapboxgl.Popup({
     closeButton: false,
-    closeOnClick: true,
+    closeOnClick: false, // clé pour mobile : ne pas fermer au tap
     offset: 10
   })
 
+  // Désactive le double-tap zoom sur mobile
+  if ('ontouchstart' in window) {
+    this.doubleClickZoom.disable()
+  }
+
   this.parcelles.forEach((parcelle, index) => {
-    this.on('mouseenter', 'background' + index, (e) => {
-      this.getCanvas().style.cursor = 'pointer'
-    })
+    // Hover uniquement desktop
+    if (!('ontouchstart' in window)) {
+      this.on('mouseenter', 'background' + index, () => {
+        this.getCanvas().style.cursor = 'pointer'
+      })
+      this.on('mouseleave', 'background' + index, () => {
+        this.getCanvas().style.cursor = ''
+        popup.remove()
+      })
+    }
 
+    // Click / tap
     this.on('click', 'background' + index, (e) => {
-      this.getCanvas().style.cursor = 'pointer'
-      popup
-      .setLngLat(parcelle.getCentroid())
-      .setHTML(new PopupContent(parcelle).create())
-      .addTo(this)
-    })
+      e.preventDefault()
+      e.originalEvent.stopPropagation()
 
-    this.on('mouseleave', 'background' + index, () => {
-      this.getCanvas().style.cursor = ''
-      popup.remove()
+      // Affiche la popup
+      popup
+        .setLngLat(parcelle.getCentroid())
+        .setHTML(new PopupContent(parcelle).create())
+        .addTo(this)
     })
   })
 }
